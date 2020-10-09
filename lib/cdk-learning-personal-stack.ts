4b1b1f7ab49e04f7d3329a5a5b90e6feb2b6601e
Kupke <lincoln.kupke@libertymutual.com>
2020-10-09 11:45:22 -0400

    Removal of boiler plate code

--- lib/cdk-learning-personal-stack.ts
+++ lib/cdk-learning-personal-stack.ts
@@ -7,12 +7,6 @@ export class CdkLearningPersonalStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkLearningPersonalQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'CdkLearningPersonalTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    
   }
 }
